a963be6a7bc77c1f8cc7be00479fc75174a268ed
Adrien <adrienforce@icloud.com>
2024-02-19 19:28:23 +0100

    first commit on new repo

---
+++ models/CommentManager.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Cette classe sert à gérer les commentaires. 
+ */
+class CommentManager extends AbstractEntityManager
+{
+    /**
+     * Récupère le nombre de commentaires d'un article.
+     * @param int $idArticle : l'id de l'article.
+     * @return int : le nombre de commentaires.
+     */
+    public function getNbCommentsByArticleId(int $idArticle) : int
+    {
+
+        //TODO Syntaxe SQL retour a ligne
+        $sql = <<<SQL
+        SELECT COUNT(*) FROM comment WHERE id_article = :idArticle
+        SQL;
+        $result = $this->db->query($sql, ['idArticle' => $idArticle]);
+        return $result->fetchColumn();
+    }
+
+    /**
+     * Récupère tous les commentaires d'un article.
+     * @param int $idArticle : l'id de l'article.
+     * @return array : un tableau d'objets Comment.
+     */
+    public function getAllCommentsByArticleId(int $idArticle) : array
+    {
+        $sql = <<<SQL
+        SELECT * FROM comment WHERE id_article = :idArticle
+        SQL;
+        $result = $this->db->query($sql, ['idArticle' => $idArticle]);
+        $comments = [];
+
+        while ($comment = $result->fetch()) {
+            $comments[] = new Comment($comment);
+        }
+        return $comments;
+    }
+
+    /**
+     * Récupère un commentaire par son id.
+     * @param int $id : l'id du commentaire.
+     * @return Comment|null : un objet Comment ou null si le commentaire n'existe pas.
+     */
+    public function getCommentById(int $id) : ?Comment
+    {
+        $sql = <<<SQL
+        SELECT * FROM comment WHERE id = :id
+        SQL;
+        $result = $this->db->query($sql, ['id' => $id]);
+        $comment = $result->fetch();
+        if ($comment) {
+            return new Comment($comment);
+        }
+        return null;
+    }
+
+    /**
+     * Ajoute un commentaire.
+     * @param Comment $comment : l'objet Comment à ajouter.
+     * @return bool : true si l'ajout a réussi, false sinon.
+     */
+    public function addComment(Comment $comment) : bool
+    {
+        $sql = "INSERT INTO comment (pseudo, content, id_article, date_creation) VALUES (:pseudo, :content, :idArticle, NOW())";
+        $result = $this->db->query($sql, [
+            'pseudo' => $comment->getPseudo(),
+            'content' => $comment->getContent(),
+            'idArticle' => $comment->getIdArticle()
+        ]);
+        return $result->rowCount() > 0;
+    }
+
+    /**
+     * Supprime un commentaire.
+     * @param Comment $comment : l'objet Comment à supprimer.
+     * @return bool : true si la suppression a réussi, false sinon.
+     */
+    public function deleteComment(Comment $comment) : bool
+    {
+        $sql = "DELETE FROM comment WHERE id = :id";
+        $result = $this->db->query($sql, ['id' => $comment->getId()]);
+        return $result->rowCount() > 0;
+    }
+
+}
