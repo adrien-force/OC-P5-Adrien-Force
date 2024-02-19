c439c0298be9cbf6a90ddb7c6286aa2342d0b25e
Adrien <adrienforce@icloud.com>
2024-02-19 19:24:56 +0100

    first commit on new repo

---
+++ models/ArticleManager.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+ * Classe qui gère les articles.
+ */
+class ArticleManager extends AbstractEntityManager 
+{
+    /**
+     * Récupère tous les articles.
+     * @return array : un tableau d'objets Article.
+     */
+    public function getAllArticles() : array
+    {
+        $sql = <<<SQL
+        SELECT * FROM article
+        SQL;
+        $result = $this->db->query($sql);
+        $articles = [];
+
+        while ($article = $result->fetch()) {
+            $articles[] = new Article($article);
+        }
+        return $articles;
+    }
+
+    /**
+     * Récupère tous les articles avec le nombre de commentaires associés.
+     * @param string $sortData : la colonne de tri.
+     * @param string $sortOrder : l'ordre de tri.
+     * @return array : un tableau d'objets Article.
+     */
+    public function getAllArticlesWithNbComments(string $sortData = null, string $sortOrder = null) : array
+    {
+
+        //TODO Mapping sortOrder / anti injection SQL
+        
+        $columnMapping = [
+            'id' => 'article.id',
+            'title' => 'article.title',
+            'content' => 'article.content',
+            'date_creation' => 'article.date_creation',
+            'date_update' => 'article.date_update',
+            'id_user' => 'article.id_user',
+            'views' => 'article.views',
+            'nbComments' => 'COUNT(comment.id)'
+        ];
+        
+        $sql =
+        <<<'SQL'
+        SELECT article.*, COUNT(comment.id) AS nbComments 
+        FROM article 
+        LEFT JOIN comment ON article.id = comment.id_article 
+        GROUP BY article.id
+        SQL;
+        
+        if ($sortData && $sortOrder) {
+            if (array_key_exists($sortData, $columnMapping)) {
+                $sql .= " ORDER BY {$columnMapping[$sortData]} {$sortOrder}";
+            }
+        }
+        
+        $result = $this->db->query($sql);
+        $articles = [];
+
+        while ($article = $result->fetch()) {
+            $articles[] = new Article($article);
+        }
+        return $articles;
+    }
+    /**
+     * Récupère un article par son id.
+     * @param int $id : l'id de l'article.
+     * @return Article|null : un objet Article ou null si l'article n'existe pas.
+     */
+    public function getArticleById(int $id) : ?Article
+    {
+        $sql = <<<SQL
+        SELECT * FROM article WHERE id = :id
+        SQL;
+        $result = $this->db->query($sql, ['id' => $id]);
+        $article = $result->fetch();
+        if ($article) {
+            return new Article($article);
+        }
+        return null;
+    }
+
+    /**
+     * Ajoute ou modifie un article.
+     * On sait si l'article est un nouvel article car son id sera -1.
+     * @param Article $article : l'article à ajouter ou modifier.
+     * @return void
+     */
+    public function addOrUpdateArticle(Article $article) : void 
+    {
+        if ($article->getId() == -1) {
+            $this->addArticle($article);
+        } else {
+            $this->updateArticle($article);
+        }
+    }
+
+    /**
+     * Augmente de 1 le nombre de vues d'un article.
+     * @param int $id : l'id de l'article.
+     * @return void
+     */
+    public function increaseViews(int $id) : void
+    {
+        $sql = "UPDATE article SET views = views + 1 WHERE id = :id";
+        $this->db->query($sql, ['id' => $id]);
+    }
+
+    /**
+     * Ajoute un article.
+     * @param Article $article : l'article à ajouter.
+     * @return void
+     */
+    public function addArticle(Article $article) : void
+    {
+        $sql = "INSERT INTO article (id_user, title, content, date_creation) VALUES (:id_user, :title, :content, NOW())";
+        $this->db->query($sql, [
+            'id_user' => $article->getIdUser(),
+            'title' => $article->getTitle(),
+            'content' => $article->getContent()
+        ]);
+    }
+
+    /**
+     * Modifie un article.
+     * @param Article $article : l'article à modifier.
+     * @return void
+     */
+    public function updateArticle(Article $article) : void
+    {
+        $sql = "UPDATE article SET title = :title, content = :content, date_update = NOW() WHERE id = :id";
+        $this->db->query($sql, [
+            'title' => $article->getTitle(),
+            'content' => $article->getContent(),
+            'id' => $article->getId()
+        ]);
+    }
+
+    /**
+     * Supprime un article.
+     * @param int $id : l'id de l'article à supprimer.
+     * @return void
+     */
+    public function deleteArticle(int $id) : void
+    {
+        $sql = "DELETE FROM article WHERE id = :id";
+        $this->db->query($sql, ['id' => $id]);
+    }
+}
---
+++ views/templates/adminData.php
@@ -0,0 +1,69 @@
+<?php
+
+/** 
+ * Affichage de la partie admin : liste des articles avec un bouton "modifier" pour chacun. 
+ * Et un formulaire pour ajouter un article. 
+ */
+
+// TODO Typage function
+// TODO Mettre la function dans Utils.php ??
+function sortLinkSelector($input) {
+    if (isset($_GET['sortOrder']) || (isset($_GET['sortData']))) {
+        if ($_GET['sortData'] == $input && $_GET['sortOrder'] == 'ASC') {
+            $output = 'DESC';
+        } else {
+            $output = 'ASC';
+        }
+    } else {
+        $output = 'ASC';
+    }
+    return $output;
+}
+
+?>
+
+<div class="adminLinks"> 
+    <h2> <a href="index.php?action=admin" > Edition des articles </a></h2>
+    <h2> <a href="index.php?action=adminData" > Panel Data  </a></h2>
+</div>
+
+<div class="adminArticle">
+
+    <table>
+        <tr>
+            <!--! TODO Refactor le selecteur de lien -->
+            <th>
+                <a href="index.php?action=adminData&sortData=title&sortOrder=<?= sortLinkSelector('title') ?>"> Titre </a>
+            </th>
+            <th>
+                <a href="index.php?action=adminData&sortData=views&sortOrder=<?= sortLinkSelector('views') ?>"> Nombres de vues </a>
+            </th>
+            <th>
+                <a href="index.php?action=adminData&sortData=nbComments&sortOrder=<?= sortLinkSelector('nbComments') ?>"> Nombre de commentaires </a>
+            </th>
+            <th>
+                <a href="index.php?action=adminData&sortData=date_creation&sortOrder=<?= sortLinkSelector('date_creation') ?>"> Date de creation </a>
+            </th>
+            <th>
+                <a href="index.php?action=adminData&sortData=date_update&sortOrder=<?= sortLinkSelector('date_update') ?>"> Date de mise à jour </a>
+            </th>
+            <th></th>
+            <th></th>
+        </tr>
+
+        
+        <?php foreach ($articles as $index => $article) { ?>
+            <tr class="<?= $index % 2 == 0 ? 'even' : 'odd' ?>">
+                <td><?= $article->getTitle() ?></td>
+                <td><?= $article->getViews() ?></td>
+                <td><?= $article->getNbComments() ?></td>
+                <td><?= $article->getDateCreationString()  ?></td>
+                <td><?= $article->getDateUpdateString() ?></td>
+                <td><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></td>
+                <td><a class="submit" href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?>>Supprimer</a></td>
+            </tr>
+        <?php }  ?>
+    </table>
+</div>
+
+<a class="submit" href="index.php?action=showUpdateArticleForm">Ajouter un article</a>
