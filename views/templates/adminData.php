de3f85b458fa1e36c03b082d96e7ce8c4be239fb
Adrien <adrienforce@icloud.com>
2024-02-26 22:18:31 +0100

    refacto : rm var_dump + refact adminData.php

--- views/templates/adminData.php
+++ views/templates/adminData.php
@@ -1,13 +1,12 @@
 <?php
 
-/** 
- * Affichage de la partie admin : liste des articles avec un bouton "modifier" pour chacun. 
- * Et un formulaire pour ajouter un article. 
+/**
+ * Affichage de la partie admin : liste des articles avec un bouton "modifier" pour chacun.
+ * Et un formulaire pour ajouter un article.
  */
 
-var_dump($_SESSION);
-
-function sortLinkSelector(string $input): string {
+function sortLinkSelector(string $input): string
+{
     if (isset($_GET['sortOrder']) || (isset($_GET['sortData']))) {
         if ($_GET['sortData'] == $input && $_GET['sortOrder'] == 'ASC') {
             $output = 'DESC';
@@ -22,9 +21,9 @@ function sortLinkSelector(string $input): string {
 
 ?>
 
-<div class="adminLinks"> 
-    <h2> <a href="index.php?action=admin" > Edition des articles </a></h2>
-    <h2> <a href="index.php?action=adminData" > Panel Data  </a></h2>
+<div class="adminLinks">
+    <h2><a href="index.php?action=admin"> Edition des articles </a></h2>
+    <h2><a href="index.php?action=adminData"> Panel Data </a></h2>
 </div>
 
 <div class="adminArticle">
@@ -32,19 +31,24 @@ function sortLinkSelector(string $input): string {
     <table>
         <tr>
             <th>
-                <a href="index.php?action=adminData&sortData=title&sortOrder=<?= sortLinkSelector('title') ?>"> Titre </a>
+                <a href="index.php?action=adminData&sortData=title&sortOrder=<?= sortLinkSelector('title') ?>">
+                    Titre </a>
             </th>
             <th>
-                <a href="index.php?action=adminData&sortData=views&sortOrder=<?= sortLinkSelector('views') ?>"> Nombres de vues </a>
+                <a href="index.php?action=adminData&sortData=views&sortOrder=<?= sortLinkSelector('views') ?>"> Nombres
+                    de vues </a>
             </th>
             <th>
-                <a href="index.php?action=adminData&sortData=nbComments&sortOrder=<?= sortLinkSelector('nbComments') ?>"> Nombre de commentaires </a>
+                <a href="index.php?action=adminData&sortData=nbComments&sortOrder=<?= sortLinkSelector('nbComments') ?>">
+                    Nombre de commentaires </a>
             </th>
             <th>
-                <a href="index.php?action=adminData&sortData=date_creation&sortOrder=<?= sortLinkSelector('date_creation') ?>"> Date de creation </a>
+                <a href="index.php?action=adminData&sortData=date_creation&sortOrder=<?= sortLinkSelector('date_creation') ?>">
+                    Date de creation </a>
             </th>
             <th>
-                <a href="index.php?action=adminData&sortData=date_update&sortOrder=<?= sortLinkSelector('date_update') ?>"> Date de mise à jour </a>
+                <a href="index.php?action=adminData&sortData=date_update&sortOrder=<?= sortLinkSelector('date_update') ?>">
+                    Date de mise à jour </a>
             </th>
             <th></th>
             <th></th>
@@ -53,18 +57,22 @@ function sortLinkSelector(string $input): string {
 
         </tr>
 
-        
+
         <?php if (isset($articles)) {
             foreach ($articles as $index => $article) { ?>
                 <tr class="<?= $index % 2 == 0 ? 'even' : 'odd' ?>">
                     <td><?= $article->getTitle() ?></td>
                     <td><?= $article->getViews() ?></td>
                     <td><?= $article->getNbComments() ?></td>
-                    <td><?= $article->getDateCreationString()  ?></td>
+                    <td><?= $article->getDateCreationString() ?></td>
                     <td><?= $article->getDateUpdateString() ?></td>
-                    <td><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Commentaires</a></td>
-                    <td><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></td>
-                    <td><a class="submit" href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?>>Supprimer</a></td>
+                    <td><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Commentaires</a>
+                    </td>
+                    <td><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a>
+                    </td>
+                    <td><a class="submit"
+                           href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?>>Supprimer</a>
+                    </td>
                 </tr>
             <?php }
         } ?>
